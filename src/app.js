25e66d3ce43dfb229c2312ec0079bf8260058469
Joel Guizar <joelguizar@gmail.com>
2017-12-31 19:50:23 -0800

    Home App Component, Redux-Promise

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-import react from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
+
+import Home from './components/Home.js';
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={Home} exact/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
